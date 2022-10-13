6488f38412440388d2d8af83d150a19f6d2f9f94
Subhankar Samanta <100989212+passion-lab@users.noreply.github.com>
2022-10-13 11:57:30 +0530

    main program file added
    main.py file added for coding the program.

---
+++ main.py
@@ -0,0 +1,20 @@
+# Importing libraries
+from tkinter import Frame, Label, Button
+
+
+# Game Board GUI class
+class Game_Board:
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+
+    def create_grid(self):
+        pass
+
+
+# Game Logic class
+
+
+# Runtime
+if __name__ == '__main__':
+    pass
